6552d1414d3139447aa280a7b9fc0d997f913ea1
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-12-16 12:11:53 +0100

    some minor changes

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # YoutubeDownloader
 
 ## Description
-This is a simple youtube downloader. It can download videos only from YouTube. 
+
+This is a simple YouTube downloader. It can download videos only from YouTube.
 
 # !!! Disclaimer !!!
+
 You can only download videos that are smaller than 6 MB. That's because of the Vercel's serverless functions limit. If you want to download bigger videos, you can follow the instructions below to host the project on your own server or computer or use my python YouTube downloader [here](https://github.com/jakecernet/YT_Downloader).
 
 ## Usage
+
 1. Copy the link of the video you want to download from the address bar of your browser or else it won't work.
 2. Paste the link in the input field.
 3. Click on the download button.
@@ -14,8 +17,19 @@ You can only download videos that are smaller than 6 MB. That's because of the V
 5. When the download is finished, you can either download the video or the audio file.
 
 ## Local installation
+
 1. Clone the repository.
 2. Run the command `npm install` in the root directory.
 3. Run the command `npm run dev` to start the development server.
 4. Open `localhost:3000` in your browser.
 5. You can use it like the online version, the only difference is that you can download bigger videos.
+
+## How it works
+
+The project is built using Express.js and vanilla JavaScript.
+It works bi utilizing the [ytdl-core](https://www.npmjs.com/package/ytdl-core) package, using which it downloads the video and audio files from YouTube.
+It then saves the downloaded to cache using the `blob` object and then creates a download link for the user to download the file.
+
+## License
+
+No license. Feel free to use it however you want, just don't claim it as your own.
--- client/index.js
+++ client/index.js
@@ -1,128 +1,137 @@
-const outputDiv = document.querySelector('.output');
-const audioMessageElement = document.getElementById('audioMessage');
-const audioLinkElement = document.getElementById('audioLink');
-const videoMessageElement = document.getElementById('videoMessage');
-const videoLinkElement = document.getElementById('videoLink');
-const videoThumbnailElement = document.getElementById('videoThumbnail')
+const outputDiv = document.querySelector(".output");
+const audioMessageElement = document.getElementById("audioMessage");
+const audioLinkElement = document.getElementById("audioLink");
+const videoMessageElement = document.getElementById("videoMessage");
+const videoLinkElement = document.getElementById("videoLink");
+const videoThumbnailElement = document.getElementById("videoThumbnail");
 
 // Hide the output div initially
-outputDiv.style.display = 'none';
+outputDiv.style.display = "none";
 
 //hide download links initially
-audioLinkElement.style.display = videoLinkElement.style.display = 'none';
+audioLinkElement.style.display = videoLinkElement.style.display = "none";
 
-form = document.getElementById('downloadForm');
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+form = document.getElementById("downloadForm");
+form.addEventListener("submit", function (event) {
+	event.preventDefault();
 });
 
 function downloadAudio() {
-    const urlInput = document.getElementById('urlInput').value;
+	const urlInput = document.getElementById("urlInput").value;
 
-    //display loading message
-    audioMessageElement.innerHTML = '<p>Downloading audio...</p>';
+	//display loading message
+	audioMessageElement.innerHTML = "<p>Downloading audio...</p>";
 
-    fetch(`/download/audio?url=${encodeURIComponent(urlInput)}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response;
-        })
-        .then(response => response.blob())
-        .then(blob => {
-            // Create a Blob URL for the blob data
-            const blobUrl = URL.createObjectURL(blob);
+	fetch(`/download/audio?url=${encodeURIComponent(urlInput)}`)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return response;
+		})
+		.then((response) => response.blob())
+		.then((blob) => {
+			// Create a Blob URL for the blob data
+			const blobUrl = URL.createObjectURL(blob);
 
-            // Set the download link attributes based on the file type
-            if (blob.type.startsWith('audio')) {
-                audioLinkElement.href = blobUrl;
-                audioLinkElement.download = `${urlInput.split('v=')[1]}.mp3`;
-                audioLinkElement.style.display = 'flex';
+			// Set the download link attributes based on the file type
+			if (blob.type.startsWith("audio")) {
+				audioLinkElement.href = blobUrl;
+				audioLinkElement.download = `${urlInput.split("v=")[1]}.mp3`;
+				audioLinkElement.style.display = "flex";
 
-                // Load audio thumbnail
-                const thumbnailUrl = `https://img.youtube.com/vi/${urlInput.split('v=')[1]}/maxresdefault.jpg`;
-                videoThumbnailElement.src = thumbnailUrl;
+				// Load audio thumbnail
+				const thumbnailUrl = `https://img.youtube.com/vi/${
+					urlInput.split("v=")[1]
+				}/maxresdefault.jpg`;
+				videoThumbnailElement.src = thumbnailUrl;
 
-                // Show the output div when audio is downloaded
-                outputDiv.style.display = 'flex';
-            }
+				// Show the output div when audio is downloaded
+				outputDiv.style.display = "flex";
+			}
 
-            // Display success message
-            audioMessageElement.innerHTML = '<p>Audio downloaded successfully.</p>';
-        })
-        .catch(error => {
-            console.error('Error:', error);
+			// Display success message
+			audioMessageElement.innerHTML =
+				"<p>Audio downloaded successfully.</p>";
+		})
+		.catch((error) => {
+			console.error("Error:", error);
 
-            // Display error message
-            audioMessageElement.innerHTML = '<p>An error occurred while downloading the audio. Please try again.</p>';
+			// Display error message
+			audioMessageElement.innerHTML =
+				"<p>An error occurred while downloading the audio. Please try again.</p>";
 
-            // Hide download links and thumbnail in case of an error
-            audioLinkElement.style.display = 'none';
+			// Hide download links and thumbnail in case of an error
+			audioLinkElement.style.display = "none";
 
-            // Show the output div even on error
-            outputDiv.style.display = 'flex';
-        });
+			// Show the output div even on error
+			outputDiv.style.display = "flex";
+		});
 
-    // Scroll to the output div
-    outputDiv.scrollIntoView({ behavior: 'smooth' });
+	// Scroll to the output div
+	outputDiv.scrollIntoView({ behavior: "smooth" });
 }
 
 function downloadVideo() {
-    const urlInput = document.getElementById('urlInput').value;
+	const urlInput = document.getElementById("urlInput").value;
 
-    //display loading message
-    videoMessageElement.innerHTML = '<p>Downloading video...</p>';
+	//display loading message
+	videoMessageElement.innerHTML = "<p>Downloading video...</p>";
 
-    fetch(`/download?url=${encodeURIComponent(urlInput)}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response;
-        })
-        .then(response => response.blob())
-        .then(blob => {
-            // Create a Blob URL for the blob data
-            const blobUrl = URL.createObjectURL(blob);
+	fetch(`/download?url=${encodeURIComponent(urlInput)}`)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return response;
+		})
+		.then((response) => response.blob())
+		.then((blob) => {
+			// Create a Blob URL for the blob data
+			const blobUrl = URL.createObjectURL(blob);
 
-            // Set the download link attributes based on the file type
-            if (blob.type.startsWith('video')) {
-                videoLinkElement.href = blobUrl;
-                videoLinkElement.download = `${urlInput.split('v=')[1]}.mp4`;
-                videoLinkElement.style.display = 'flex';
-                // Load video thumbnail
-                const thumbnailUrl = `https://img.youtube.com/vi/${urlInput.split('v=')[1]}/maxresdefault.jpg`;
-                videoThumbnailElement.src = thumbnailUrl;
+			// Set the download link attributes based on the file type
+			if (blob.type.startsWith("video")) {
+				videoLinkElement.href = blobUrl;
+				videoLinkElement.download = `${urlInput.split("v=")[1]}.mp4`;
+				videoLinkElement.style.display = "flex";
+				// Load video thumbnail
+				const thumbnailUrl = `https://img.youtube.com/vi/${
+					urlInput.split("v=")[1]
+				}/maxresdefault.jpg`;
+				videoThumbnailElement.src = thumbnailUrl;
 
-                // Show the output div when video is downloaded
-                outputDiv.style.display = 'flex';
-            } else if (blob.type.startsWith('audio')) {
-                audioLinkElement.href = blobUrl;
-                audioLinkElement.download = `${urlInput.split('v=')[1]}.mp3`;
-                audioLinkElement.style.display = 'flex';
+				// Show the output div when video is downloaded
+				outputDiv.style.display = "flex";
+			} else if (blob.type.startsWith("audio")) {
+				audioLinkElement.href = blobUrl;
+				audioLinkElement.download = `${urlInput.split("v=")[1]}.mp3`;
+				audioLinkElement.style.display = "flex";
 
-                // Show the output div when audio is downloaded
-                outputDiv.style.display = 'flex';
-            }
+				// Show the output div when audio is downloaded
+				outputDiv.style.display = "flex";
+			}
 
-            // Display success message
-            videoMessageElement.innerHTML = '<p>Video downloaded successfully.</p>';
-        })
-        .catch(error => {
-            console.error('Error:', error);
+			// Display success message
+			videoMessageElement.innerHTML =
+				"<p>Video downloaded successfully.</p>";
+		})
+		.catch((error) => {
+			console.error("Error:", error);
 
-            // Display error message
-            videoMessageElement.innerHTML = '<p>An error occurred while downloading the video. Please try again.</p>';
+			// Display error message
+			videoMessageElement.innerHTML =
+				"<p>An error occurred while downloading the video. Please try again.</p>";
 
-            // Hide download links and thumbnail in case of an error
-            videoLinkElement.style.display = audioLinkElement.style.display = 'none';
-            videoThumbnailElement.src = '';
+			// Hide download links and thumbnail in case of an error
+			videoLinkElement.style.display = audioLinkElement.style.display =
+				"none";
+			videoThumbnailElement.src = "";
 
-            // Show the output div even on error
-            outputDiv.style.display = 'flex';
-        });
+			// Show the output div even on error
+			outputDiv.style.display = "flex";
+		});
 
-    // Scroll to the output div
-    outputDiv.scrollIntoView({ behavior: 'smooth' });
+	// Scroll to the output div
+	outputDiv.scrollIntoView({ behavior: "smooth" });
 }
